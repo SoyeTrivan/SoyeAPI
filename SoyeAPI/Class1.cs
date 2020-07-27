d5db0b54a59988f8b92e7a2eb2629221ac56e89c
SoyeTrivan <68832330+SoyeTrivan@users.noreply.github.com>
2020-07-27 11:54:12 +1000

    Added files

---
+++ SoyeAPI/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoyeAPI
+{
+    public class DOJScriptHelper
+    {
+        public static string ScriptLocation()
+        {
+            string x = @"some folder on a computer somewhere";
+            return x;
+        }
+
+        public static string BotToken()
+        {
+            string x = "put Discord bot token here";
+            return x;
+        }
+    }
+}
